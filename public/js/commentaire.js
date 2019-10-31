aba748fef7946cd55d177f1643b865afca8c8693
Aurelien <linux.aurelien@gmail.com>
2019-10-31 11:32:59 +0100

    ajout de la fonctoion see media

--- public/js/commentaire.js
+++ public/js/commentaire.js
@@ -1,6 +1,22 @@
 //attend que le DOM soit chargé
 $( document ).ready(function(){
 
+    //************* Pour gerer le voir medias ***********
+    const boutonSeeMedia = $("#see-media");
+    const divMedia = $("div.row.my-3.col.mx-auto.media-trick");
+    let nbMedia = $("div.col-lg-3.col-md-6.mb-3").length;
+
+    if (nbMedia === 0){
+        boutonSeeMedia.remove();
+    }
+
+    //Permet de montrer les medias suite au click du bouton
+    boutonSeeMedia.click(function(){
+        this.style.display = "none";
+        divMedia[0].style.display = "block";
+    });
+    //*************************************************
+
     const loadMoreComment = $("#loadMoreCom");
     const loadMoreComDiv = $("#loadMoreComDiv");
     const animGifajax = $('#ajax-loading-com');
