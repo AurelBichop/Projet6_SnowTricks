cc1c0c30f714896c4a0f6a73396c1550c45694dd
Aurelien <linux.aurelien@gmail.com>
2019-11-02 10:22:51 +0100

    ajout de la suppression des tricks en administration

--- src/Controller/AdminTrickController.php
+++ src/Controller/AdminTrickController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Trick;
 use App\Service\Pagination;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,5 +28,26 @@ class AdminTrickController extends AbstractController
         return $this->render('admin/trick/index.html.twig', [
             'pagination' => $pagination
         ]);
+    }
+
+    /**
+     * Permet de supprimer une annonce
+     *
+     * @Route("/admin/tricks/{id}/delete", name="admin_tricks_delete")
+     *
+     * @param Trick $trick
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function delete(Trick $trick, ObjectManager $manager){
+        $manager->remove($trick);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "Trick supprimé"
+        );
+
+        return $this->redirectToRoute('admin_tricks_index');
     }
 }
