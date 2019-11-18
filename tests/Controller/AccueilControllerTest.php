ce72aaf048dbcfc89d64fe6d32c5ca097d8f8097
Aurelien <linux.aurelien@gmail.com>
2019-11-18 14:34:25 +0100

    ajout de la route pour load more

--- tests/Controller/AccueilControllerTest.php
+++ tests/Controller/AccueilControllerTest.php
@@ -8,11 +8,23 @@ class AccueilControllerTest extends WebTestCase{
     /**
      * Permet de tester la route de l'accueil
      */
-    public function testindex()
+    public function testIndex()
     {
         $client = static::createClient();
 
         $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Permet de tester la route pour le load more de l'accueil
+     */
+    public function testLoadMore()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/accueil/more');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
